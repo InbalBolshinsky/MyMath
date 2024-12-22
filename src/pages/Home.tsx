e3dca855ab23896ab0d6b3b165282ba08ccb4d2d
InbalBolshinsky <Inbalbolsh@gmail.com>
2024-12-22 15:45:46 +0200

    login pop up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Home.css'
-import LoginForm from '../components/LoginForm.jsx'
-
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import './Home.css';
+import { LoginForm } from './LoginForm' 
 
 export const Home = () => {
-  const handleLogin = (userName, password) => {
-    console.log("Logged in with:", userName, password);
-    // Navigate to the exercise page or save the user state
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const handleLoginSubmit = (userName: string, password: string) => {
+    if (!userName.trim() || !password.trim()) {
+      console.error("Username and password cannot be empty.");
+    }
+    else {
+    console.log("Username:", userName);
+    console.log("Password:", password);
+    }
+    setPopupOpen(false); // Close the popup after submission
+    return;
   };
 
     return (
         <div className='home-container'>
           <h1 className='my-math'>MyMath</h1>
           <p className='welcome-msg'>Welcome to MyMath!</p>
-          <LoginForm onSubmit={handleLogin} />
+          <button className='open-login' onClick={() => setPopupOpen(true)}>Open Login</button>
+          <LoginForm
+            isOpen={isPopupOpen}
+            onClose={() => setPopupOpen(false)}
+            onSubmit={handleLoginSubmit}
+          />
           <Link className="to-exercise" to="/exercise">Let's Start Learning!</Link>
         </div>
       );
